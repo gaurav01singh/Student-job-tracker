12a0d0bcf808399ad556d07f36fc9db2bb3754ab
gaurav01singh <gauravsingh02195@gmail.com>
2025-04-11 13:03:12 +0530

    Update API base URL to use production endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "student-job-tracker-backend.up.railway.app/api" });
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem("user"));
